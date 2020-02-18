1950c0dc571e977b42bb12a9a9e3ec28b4e4892b
arif <muarwi@gmail.com>
2020-02-18 14:53:09 +0800

    added Spring boot starter project

---
+++ src/main/java/com/github/wicaksana/cateringapp/CateringappApplication.java
@@ -0,0 +1,13 @@
+package com.github.wicaksana.cateringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CateringappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CateringappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/wicaksana/cateringapp/CateringappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wicaksana.cateringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CateringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
